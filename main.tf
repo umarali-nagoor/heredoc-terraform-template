8721f449ff41fa36813346ce213e8110d9a58e09
Umar Ali <umarali@Umars-MacBook-Pro.local>
2024-07-22 12:17:07 +0530

    Added SSH key

--- main.tf
+++ main.tf
@@ -3,3 +3,7 @@ data "template_file" "test" {
   template = "Hello ${var.name}"
 }
 
+data "template_file" "test2" {
+  template = "Hello ${var.ssh}"
+}
+
--- vars.tf
+++ vars.tf
@@ -4,3 +4,8 @@ variable "name" {
   description = "The name to pass to the template."
   default = "Amigos"
 }
+
+variable "ssh" {
+  description = "Repo ssh key"
+  default = "test_ssh"
+}
