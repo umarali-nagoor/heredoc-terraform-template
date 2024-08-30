c66e138c0ed3de178181606c8bb287fa0d308126
umarali-nagoor <55839118+umarali-nagoor@users.noreply.github.com>
2024-08-30 10:57:20 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
 # Create an arbitrary local resource
 data "template_file" "test" {
-  template = "Hello ${var.name}"
+  template = "${var.name}"
 }
 
 data "template_file" "test2" {
-  template = "Hello ${var.ssh}"
+  template = "${var.ssh}"
 }
 
 data "template_file" "test23" {
-  template = "Hello ${var.multiline}"
+  template = "${var.multiline}"
 }
 
