655199651848c65bbc8b105a8b2a6c695ee19704
umarali-nagoor <55839118+umarali-nagoor@users.noreply.github.com>
2024-08-30 11:29:09 +0530

    Update vars.tf

--- vars.tf
+++ vars.tf
@@ -1,16 +1,11 @@
 # Configure these variables
 
-variable "name" {
-  description = "The name to pass to the template."
-  default = "Amigos"
+variable "heredoc" {
+  description = "heredoc string."
+  default = "heredoc"
 }
 
-variable "ssh" {
-  description = "Repo ssh key"
-  default = "test_ssh"
+variable "plainmultiline" {
+  description = "Plain Multiline string"
+  default = "plainmultiline"
 } 
-
-variable "multiline" {
-  description = "Multiline string"
-  default = "Multiline value"
-}
