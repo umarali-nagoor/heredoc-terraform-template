f77719e5088f6ecc66fcd6e600c2a924c6d78e6f
umarali-nagoor <55839118+umarali-nagoor@users.noreply.github.com>
2024-08-30 10:56:48 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,7 +1,7 @@
-output "template_file" {
+output "template_file3" {
     value = data.template_file.test.rendered
 }
 
-output "template_file2" {
+output "template_file4" {
     value = data.template_file.test2.rendered
 }
