7b38bcdc2c83f8d8118f4527edd3c466c93951e4
umarali-nagoor <55839118+umarali-nagoor@users.noreply.github.com>
2024-09-02 11:45:18 +0530

    Update vars.tf

--- vars.tf
+++ vars.tf
@@ -2,10 +2,8 @@
 
 variable "heredoc" {
   description = "heredoc string."
-  default = "heredoc"
 }
 
 variable "plainmultiline" {
   description = "Plain Multiline string"
-  default = "plainmultiline"
 } 
